07d6ac510bf9ebfd3e560d01e86a3846c12c531d
Luciano Mammino <lmammino@oryzone.com>
2012-01-29 02:33:37 +0100

    fixed wrong default puppet name

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,41 +1,72 @@
-<VirtualHost *:80>
-	ServerAdmin webmaster@localhost
+# this makes puppet and vagrant shut up about the puppet group
+group { "puppet": 
+  ensure => "present", 
+}
 
-	DocumentRoot /var/www
-	<Directory />
-		Options FollowSymLinks
-		AllowOverride None
-	</Directory>
-	<Directory /var/www/>
-		Options Indexes FollowSymLinks MultiViews
-		AllowOverride All
-		Order allow,deny
-		allow from all
-	</Directory>
+# make sure the packages are up to date before beginning
+exec { "apt-get update":
+  command => "/usr/bin/apt-get update",
+}
 
-	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-	<Directory "/usr/lib/cgi-bin">
-		AllowOverride None
-		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-		Order allow,deny
-		Allow from all
-	</Directory>
+# because puppet command are not run sequentially, ensure that packages are
+# up to date before installing before installing packages, services, files, etc.
+Package { require => Exec["apt-get update"] }
+File { require => Exec["apt-get update"] }
 
-	ErrorLog /var/log/apache2/error.log
+# ensure packages are installed to create a LAMP environment
+package { "vim":
+  ensure => present,
+}
+package { "apache2":
+  ensure => present,
+}
+package { "php5":
+  ensure => present,
+}
+package { "mysql-server":
+  ensure => present,
+}
+package { "php5-mysql":
+  ensure => present,
+}
+package { "php-pear":
+  ensure => present,
+}
+package { "php5-xdebug":
+  ensure => present,
+}
 
-	# Possible values include: debug, info, notice, warn, error, crit,
-	# alert, emerg.
-	LogLevel warn
+# upgrades PEAR, installs Codesniffer and PHPUnit
+exec {"/usr/bin/pear upgrade": 
+  require => Package['php-pear']
+}
+exec { "/usr/bin/pear install PHP_Codesniffer":
+  require => [Package['php-pear'], Exec['/usr/bin/pear upgrade']]
+}
+exec { "/usr/bin/pear config-set auto_discover 1":
+  require => [Package['php-pear'], Exec['/usr/bin/pear upgrade']]
+}
+exec { "/usr/bin/pear install pear.phpunit.de/PHPUnit":
+  require => [Package['php-pear'], Exec['/usr/bin/pear config-set auto_discover 1'], Exec['/usr/bin/pear upgrade']]
+}
 
-	CustomLog /var/log/apache2/access.log combined
+# starts the apache2 service once the packages installed, and monitors changes
+# to its configuration files and reloads if necessary
+service { "apache2":
+  ensure => running,
+  enable => true,
+  require => Package['apache2'],
+  subscribe => [File["/etc/apache2/mods-enabled/rewrite.load"], File["/etc/apache2/sites-available/default"]],
+}
 
-    Alias /doc/ "/usr/share/doc/"
-    <Directory "/usr/share/doc/">
-        Options Indexes MultiViews FollowSymLinks
-        AllowOverride None
-        Order deny,allow
-        Deny from all
-        Allow from 127.0.0.0/255.0.0.0 ::1/128
-    </Directory>
+# ensures that mod_rewrite is loaded and modifies the default configuration file
+file { "/etc/apache2/mods-enabled/rewrite.load":
+  ensure => link,
+  target => "/etc/apache2/mods-available/rewrite.load",
+  require => Package['apache2'],
+}
+file { "/etc/apache2/sites-available/default":
+  ensure => present,
+  source => "/vagrant/manifests/default",
+}  
 
-</VirtualHost>
